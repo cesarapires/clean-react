39c23754ef8424c648175b42b611fd3fdfb60fb0
Cesar Pires <cesar.augusto.pires@hotmail.com>
2023-11-26 22:20:28 -0300

    feat: ensure Login with inputs with required state

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,12 +4,13 @@ import Spinner from '@/presentation/components/spinner/spinner'
 import Context from '@/presentation/contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
-  const { isLoading, errorMessage } = useContext(Context)
+  const { state, errorState } = useContext(Context)
+  const { isLoading } = state
 
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
-      {isLoading && <Spinner className={Styles.spinner} />}
-      {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
+      { isLoading && <Spinner className={Styles.spinner} /> }
+      { errorState.main && <span className={Styles.error}>{errorState.main}</span> }
     </div>
   )
 }
